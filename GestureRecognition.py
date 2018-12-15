ddd51ea8f23a47a77904da68b87b7202192e1839
KiranCNayak <ayushpant1998@gmail.com>
2018-12-16 02:00:47 +0530

    Final Update

--- GestureRecognition.py
+++ GestureRecognition.py
@@ -2,19 +2,16 @@ import cv2
 import numpy as np
 import copy
 import math
-import pyautogui
+import sqlite3
 from collections import deque
 # import predict_2
 # import predict_1
 import predict_interface_usage as piu
-from PIL import ImageTk, Image, ImageDraw
-import PIL
 from tkinter import *
-import tkinter as tk
 
-cap_region_x_begin=0.5  # start point/total width
-cap_region_y_end=0.8  # start point/total width
-threshold = 24  #  BINARY threshold
+cap_region_x_begin = 0.5  # start point/total width
+cap_region_y_end = 0.8  # start point/total width
+threshold = 24  # BINARY threshold
 blurValue = 41  # GaussianBlur parameter
 bgSubThreshold = 50
 
@@ -23,26 +20,44 @@ end = (0, 0)
 start = (0, 0)
 learningRate = 0
 
-isBgCaptured = 0   # bool, whether the background captured
+isBgCaptured = 0  # bool, whether the background captured
 triggerSwitch = False  # if true, keyborad simulator works
 
-def printThreshold(thr):
-    print("! Changed threshold to "+str(thr))
+num = 0
 
-def removeBG(frame):
-    fgmask = bgModel.apply(frame,learningRate=learningRate)
+conn = sqlite3.connect('test.db')
+con = sqlite3.connect('database.db')
+curr = conn.cursor()
+cur = con.cursor()
+curr.execute("""CREATE TABLE IF NOT EXISTS PREV(VAL INTEGER NOT NULL)""")
+cur.execute("""CREATE TABLE IF NOT EXISTS NUMBER(VAL INTEGER NOT NULL)""")
+
+if curr and cur:
+    print("DBs Created Successfully")
+
+curr.execute("SELECT * FROM PREV")
+cursor = curr.fetchall()
+for row in cursor:
+    if row[0] >= 0:
+        num = row[0]
+    else:
+        num = 0
+
+
+def remove_bg(frame):
+    fgmask = bgModel.apply(frame, learningRate=learningRate)
     kernel = np.ones((3, 3), np.uint8)
     fgmask = cv2.erode(fgmask, kernel, iterations=1)
     res = cv2.bitwise_and(frame, frame, mask=fgmask)
     return res
 
-def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
+
+def calculate_fingers(res, drawing):  # -> finished bool, cnt: finger count
     hull = cv2.convexHull(res, returnPoints=False)
     if len(hull) > 3:
         defects = cv2.convexityDefects(res, hull)
         if type(defects) != type(None):  # avoid crashing.   (BUG not found)
             cnt = 0
-            pointList = []
             for i in range(defects.shape[0]):  # calculate the angle
                 global far
                 global end
@@ -61,12 +76,11 @@ def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
             return True, cnt
     return False, 0
 
+
 camera = cv2.VideoCapture(0)
 
-camera.set(cv2.CAP_PROP_FRAME_WIDTH,600)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT,500)
-cv2.namedWindow('trackbar')
-cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
 blueLower = np.array([100, 60, 60])
 blueUpper = np.array([140, 255, 255])
 kernel = np.ones((5, 5), np.uint8)
@@ -81,25 +95,25 @@ gindex = 0
 rindex = 0
 yindex = 0
 
-colors = [(0, 0, 0),]
+colors = [(0, 0, 0), ]
 colorIndex = 0
-
-global paintWindow
-paintWindow = np.zeros((400,400,3)) + 255
+paintWindow = np.zeros((400, 400, 3)) + 255
 cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
 count = 0
-num = 0
+lis = []
+
 while camera.isOpened():
     ret, frame = camera.read()
-    threshold = cv2.getTrackbarPos('trh1', 'trackbar')
     frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
     frame = cv2.flip(frame, 1)  # flip the frame horizontally
-    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0), (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
     if isBgCaptured == 1:  # this part wont run until background captured
 
-        img = removeBG(frame)
+        img = remove_bg(frame)
 
-        img = img[0:int(cap_region_y_end * frame.shape[0]), int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+        img = img[0:int(cap_region_y_end * frame.shape[0]),
+              int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
         ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
@@ -107,6 +121,7 @@ while camera.isOpened():
         _, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         length = len(contours)
         maxArea = -1
+        dr = np.zeros(img.shape, np.uint8)
         if length > 0:
             for i in range(length):  # find the biggest contour (according to area)
                 temp = contours[i]
@@ -117,21 +132,26 @@ while camera.isOpened():
             res = contours[ci]
             hull = cv2.convexHull(res)
             drawing = np.zeros(img.shape, np.uint8)
-            cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2) # Outline of hands
-            cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)# Dots at the depression point
-            isFinishCal, cnt = calculateFingers(res,drawing)
+            dr = drawing
+            cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)  # Outline of hands
+            cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)  # Dots at the depression point
+            isFinishCal, cnt = calculate_fingers(res, drawing)
             if triggerSwitch is True:
                 if isFinishCal is True and cnt <= 5:
-                    print (cnt+1)
+                    print(cnt + 1)
 
-        cv2.imshow('contour output', drawing)
-        _, count = calculateFingers(res,drawing)
-        if(count+1 >1 and count+1 < 3 ):# Checking if the no. of fingers is 2 if not then dont draw
+        cntr_output_window_name = "contour output"
+        cv2.namedWindow(cntr_output_window_name)  # Create a named window
+        cv2.moveWindow(cntr_output_window_name, 20, 40)
+        cv2.imshow(cntr_output_window_name, dr)
+
+        _, count = calculate_fingers(res, dr)
+        if 1 < count + 1 < 3:  # Checking if the no. of fingers is 2 if not then dont draw
             if len(contours) > 0:
 
-            	# Sort the contours and find the largest one -- we
-            	# will assume this contour correspondes to the area of the bottle cap
-                cnt = sorted(contours, key = cv2.contourArea, reverse = True)[0]
+                # Sort the contours and find the largest one -- we
+                # will assume this contour corresponds to the area of the bottle cap
+                cnt = sorted(contours, key=cv2.contourArea, reverse=True)[0]
                 # Get the radius of the enclosing circle around the found contour
                 ((x, y), radius) = cv2.minEnclosingCircle(cnt)
                 # Draw the circle around the contour
@@ -141,7 +161,7 @@ while camera.isOpened():
                 center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
 
                 if center[1] <= 65:
-                    if 40 <= center[0] <= 140: # Clear All
+                    if 40 <= center[0] <= 140:  # Clear All
                         bpoints = [deque(maxlen=512)]
                         gpoints = [deque(maxlen=512)]
                         rpoints = [deque(maxlen=512)]
@@ -152,16 +172,16 @@ while camera.isOpened():
                         rindex = 0
                         yindex = 0
 
-                        paintWindow[67:,:,:] = 255
+                        paintWindow[67:, :, :] = 255
                     elif 160 <= center[0] <= 255:
-                            colorIndex = 0 # Blue
+                        colorIndex = 0  # Blue
                     elif 275 <= center[0] <= 370:
-                            colorIndex = 1 # Green
+                        colorIndex = 1  # Green
                     elif 390 <= center[0] <= 485:
-                            colorIndex = 2 # Red
+                        colorIndex = 2  # Red
                     elif 505 <= center[0] <= 600:
-                            colorIndex = 3 # Yellow
-                else :
+                        colorIndex = 3  # Yellow
+                else:
                     if colorIndex == 0:
                         bpoints[bindex].appendleft(center)
                     elif colorIndex == 1:
@@ -192,16 +212,54 @@ while camera.isOpened():
                         cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 10)
 
         # Show the frame and the paintWindow image
-        cv2.imshow("Tracking", frame)
-        cv2.imshow("Paint", paintWindow)
+        tracking_window_name = "Tracking"
+        cv2.namedWindow(tracking_window_name)  # Create a named window
+        cv2.moveWindow(tracking_window_name, 660, 40)  # Move it to (40,30)
+        cv2.imshow(tracking_window_name, frame)
+
+        paint_window_name = "Paint"
+        cv2.namedWindow(paint_window_name)  # Create a named window
+        cv2.moveWindow(paint_window_name, 420, 40)  # Move it to (40,30)
+        cv2.imshow(paint_window_name, paintWindow)
 
     # Keyboard OP
     k = cv2.waitKey(10)
 
     if k == 27:  # press ESC to exit
+        conn.commit()
+        conn.close()
+        cursor = cur.execute("SELECT * FROM NUMBER")
+        for row in cursor:
+            lis.append(row[0])
+        cur.execute("DROP TABLE NUMBER")
+        su = 0
+
+        for i in lis:
+            su = su + i
+
+        if su == 0:
+            con.commit()
+            con.close()
+            lis.clear()
+            exit(0)
+        a = Tk()
+        a.geometry("500x200")
+        a.title("Predicted Value")
+        f = Frame(a)
+        f.grid()
+        lbl = Label(f, font=("Helvetika", 50))
+        lbl.grid(row=0, column=0)
+        lbl['text'] = "+".join([str(x) for x in lis])
+        lbl['text'] += "\nSum: " + str(su)
+        a.after(4000, lambda: a.destroy())  # Destroy the widget after 30 seconds
+        a.mainloop()
+
+        con.commit()
+        con.close()
+        lis.clear()
         break
 
-    if k ==ord('c'):#press 'c' to Clear Screen
+    if k == ord('c'):  # press 'c' to Clear Screen
 
         bpoints = [deque(maxlen=512)]
         gpoints = [deque(maxlen=512)]
@@ -212,31 +270,46 @@ while camera.isOpened():
         gindex = 0
         rindex = 0
         yindex = 0
-        paintWindow[67:,:,:] = 255
+        paintWindow[67:, :, :] = 255
         print("SCREEN CLEARED")
 
     elif k == ord('b'):  # press 'b' to capture the background
         bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
         isBgCaptured = 1
-        print( '!!!Background Captured!!!')
+        print('Background Captured')
 
     elif k == ord('r'):  # press 'r' to reset the background
         bgModel = None
         triggerSwitch = False
         isBgCaptured = 0
-        print ('!!!Reset BackGround!!!')
 
     elif k == ord('n'):
         triggerSwitch = True
 
-    elif k ==ord('s'):#press 's' to Save and clear screen
-        st = "img/img{}.png".format(count)
-        count = count + 1
-        crop_img = paintWindow[190:390, 20:250]
+    elif k == ord('s'):  # press 's' to Save and clear screen
+        st = "img/img{}.png".format(num)
+        num = num + 1
+        curr.execute("UPDATE PREV SET VAL = {}".format(num))
+        conn.commit()
+        crop_img = paintWindow[180:380, 20:220]
         cv2.imwrite(st, crop_img)
-        piu.train_and_obtain_number(st[4:-4]) #To get the name without the .jpg or .png extension
-        value = piu.getPred()
-        cv2.putText(frame, str(value), (0, 0), cv2.FONT_HERSHEY_SIMPLEX, 3, 255)
+        val = piu.train_and_obtain_number(st[4:-4])  # To get the name without the .png extension
+
+        if val != -1:
+            cur.execute("INSERT INTO NUMBER VALUES({})".format(val))
+            con.commit()
+
+        a = Tk()
+        a.geometry("130x170")
+        a.title("Predicted Value")
+        f = Frame(a)
+        f.grid()
+        lbl = Label(f, font=("Helvetika", 100))
+        lbl.grid(row=0, column=0)
+        lbl['text'] = val
+        a.after(2000, lambda: a.destroy())  # Destroy the widget after 30 seconds
+        a.mainloop()
+
         bpoints = [deque(maxlen=512)]
         gpoints = [deque(maxlen=512)]
         rpoints = [deque(maxlen=512)]
@@ -247,5 +320,5 @@ while camera.isOpened():
         rindex = 0
         yindex = 0
 
-        paintWindow[67:,:,:] = 255
+        paintWindow[67:, :, :] = 255
         print("SCREEN SAVED")
--- predict_interface_usage.py
+++ predict_interface_usage.py
@@ -1,31 +1,22 @@
 import predict_interface
-import numpy as np
-import sys
 
 """
 How to use the predict_interface -interface
 """
 
-val = 0
+val = -1
+
+
 def train_and_obtain_number(img_source):
 	global val
-	retVals = []
 	ret_vals = predict_interface.pred_from_img(img_source, False)
 
-	print(" ")
 	print("----------")
 
-	for i in range(0, len(ret_vals)):
+	for i in range(len(ret_vals)):
 		print("Prediction: " + str(ret_vals[i].get_prediction()) + ", Probability: " + str(ret_vals[i].get_probability()))
 		val = ret_vals[i].get_prediction()
-		print("Location in image: x(" + str(ret_vals[i].get_location()[1]) + "), y(" + str(ret_vals[i].get_location()[0]) + ")")
-		print("Top left corner (shifted image): x(" + str(ret_vals[i].get_top_left()[1]) + "), y(" + str(ret_vals[i].get_top_left()[0]) + ")")
-		print("Bottom right corner (shifted image): x(" + str(ret_vals[i].get_bottom_right()[1]) + "), y(" + str(ret_vals[i].get_bottom_right()[0]) + ")")
-		print("Actual width and height (cropped image): w(" + str(ret_vals[i].get_actual_w_h()[1]) + "), h(" + str(ret_vals[i].get_actual_w_h()[0]) + ")")
-		print(" ")
-		print("----------")
+		return val
+	else:
+		return -1
 
-	print("A modified image with the predictions: pro-img/IMAGE_NAME_digitized_image.png")
-
-def getPred():
-	return val
